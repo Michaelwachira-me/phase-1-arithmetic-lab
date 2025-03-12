16618eacb3ab71c93bab41989ef6c1f86a1a71c9
michaelwachira707@gmail.com <michaelwachira707@gmail.com>
2025-03-12 07:20:51 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+const num1 = 8;
+const num2 = 7.75;
+const num3 = 10;
+const num4 = 6;
+const num5 = 10;
+const num6 = 0;
+const num7 = 20;
+
+// Challenge 1: multiply
+const multiply = num1 * num2;
+
+// Challenge 2: random
+const random = Math.floor(Math.random() * 10) + 1; // Generates a random integer between 1 and 10
+
+// Challenge 3: mod
+const mod = num3 % num4;
+
+// Challenge 4: max
+const max = Math.max(num1, num2, num3, num4, num5, num6, num7);
